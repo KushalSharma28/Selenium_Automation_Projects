d735e383f09922d4930a3bdb0e244cd47410e2d1
Kushal Sharma <84232969+KushalSharma28@users.noreply.github.com>
2024-10-11 23:36:00 +0530

    Update AdminConsole.java

--- AdminConsole.java
+++ AdminConsole.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AdminConsole 
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException
 	{
 		//Chrome Webdriver Path
 		System.setProperty("webdriver.chrome.driver", "C:/Users/Supporting files/chromedriver.exe");
@@ -22,33 +22,39 @@ public class AdminConsole
 		//FirefoxDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 						
-		//epsweb webpage
+		// EPS Website
 		driver.get("https://epsweb.adminconsole.net/");
-
-		//epsweb login
+                Thread.sleep(5000);
+		
+		// EPS Website login
 		driver.findElement(By.name("EmailId")).sendKeys("santoshs@npav.net");
+		Thread.sleep(1000);
 		driver.findElement(By.name("Password")).sendKeys("cclm789");
+		Thread.sleep(2000);
 		driver.findElement(By.id("btnSubmit")).click();
-		thread.sleep(5000);
+		Thread.sleep(10000);
 
-		//click on the menu toggle
+		// Click on the Menu toggle
 		driver.findElement(By.xpath("/html/body/div[2]/header/nav/div[1]/ul/li/[i]/a/i")).click();
+		Thread.sleep(3000);
 
-		//select key-details tab
+		// Select Key-details tab
 		driver.findElement(By.xpath("/html/body/div[2]/aside/section/ul/li[2]/a/span")).click();
-
-		// search the user in key details
+		Thread.sleep(8000);
+		
+		// Search the user in key details
 		driver.findElement(By.xpath("//*[@id=\"tblKeyDet_filter\"]/label/input")).sendKeys("V-EFCA8EA698");
-		driver.findElement(By.cssSelector(".#tblKeyDet_filter")).sendKeys("V-EFCA8EA698");
+		Thread.sleep(3000);
+		//driver.findElement(By.cssSelector(".#tblKeyDet_filter")).sendKeys("V-EFCA8EA698");
 
 		// Select Key using checkbox		
-		Thread.sleep(3000);
 		driver.findElement(By.cssSelector(".SelectedChk")).click();
-
+		Thread.sleep(2000);
 		// Select Policy for Key
 		
 		//driver.findElement(By.cssSelector(".select2-selection__arrow")).click();
 	    	driver.findElement(By.id("select2-ddlpolicy-container")).click();
+		Thread.sleep(2000);
 	    
 		//WebElement element = driver.findElement(By.id("select2-ddlpolicy-container"));
 		//element.click();
@@ -56,24 +62,18 @@ public class AdminConsole
 	    
 		//driver.findElement(By.id("select2-ddlpolicy-container")).text("KushalS Null Policy");
 		driver.findElement(By.cssSelector("select2-search__field")).sendKeys("KushalS Null Policy");
-		
+		Thread.sleep(3000);
 		
 		//select2-ddlpolicy-result-ib7e-1209
 		
 		// logout from profile Menu
-		//driver.findElement(By.id("imagelogout")).click();
-		//Thread.sleep(3000);
-		//driver.findElement(By.cssSelector(".ti-lock.text-muted.mr-2")).click();
+		driver.findElement(By.id("imagelogout")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector(".ti-lock.text-muted.mr-2")).click();
 	    
-	    Thread.sleep(3000);
-	    driver.close();
+	    	Thread.sleep(3000);
+	    	driver.close();																     }
 		
-		// driver.findElement(By.xpath("//*[@id=\"V-EFCA8EA698\"]")).click();
-		// WebElement checkBox = driver.findElement(By.id("V-EFCA8EA698")); 
-																     }
-		// logout from profile Menu
-		//driver.findElement(By.xpath("//*[@id="menuss"]")).click();
-		//driver.findElement(By.xpath("//*[@id="menuss"]/li/a[5]/i")).click();
 	}
 
 }
