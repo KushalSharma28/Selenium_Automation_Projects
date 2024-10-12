f6d5625beffe44767651f8bb503fc4dc57a85598
Kushal Sharma <84232969+KushalSharma28@users.noreply.github.com>
2024-10-12 11:44:59 +0530

    Update AdminConsole.java

--- AdminConsole.java
+++ AdminConsole.java
@@ -28,8 +28,10 @@ public class AdminConsole
 		
 		// EPS Website login
 		driver.findElement(By.name("EmailId")).sendKeys("santoshs@npav.net");
+		// driver.findElement(By.name("EmailId")).sendKeys("santoshs@npav.net");
 		Thread.sleep(1000);
 		driver.findElement(By.name("Password")).sendKeys("cclm789");
+		// driver.findElement(By.name("Password")).sendKeys("cclm789");
 		Thread.sleep(2000);
 		driver.findElement(By.id("btnSubmit")).click();
 		Thread.sleep(10000);
@@ -44,6 +46,7 @@ public class AdminConsole
 		
 		// Search the user in key details
 		driver.findElement(By.xpath("//*[@id=\"tblKeyDet_filter\"]/label/input")).sendKeys("V-EFCA8EA698");
+		// driver.findElement(By.xpath("//*[@id=\"tblKeyDet_filter\"]/label/input")).sendKeys("V-1EC04AE979");
 		Thread.sleep(3000);
 		//driver.findElement(By.cssSelector(".#tblKeyDet_filter")).sendKeys("V-EFCA8EA698");
 
@@ -61,7 +64,20 @@ public class AdminConsole
 		//element.sendKeys("KushalS Null Policy");
 	    
 		//driver.findElement(By.id("select2-ddlpolicy-container")).text("KushalS Null Policy");
-		driver.findElement(By.cssSelector("select2-search__field")).sendKeys("KushalS Null Policy");
+		//driver.findElement(By.cssSelector("select2-search__field")).sendKeys("KushalS Null Policy");
+		
+		// Click on Dropdown
+		WebElement dropdown = driver.findElement(By.id("select2-ddlpolicy-container"));
+		dropdown.click();
+		Thread.sleep(4000);
+		
+		// Select the Dropdown value
+		WebElement option = driver.findElement(By.xpath("//option[@value='1209']"));
+		// WebElement option = driver.findElement(By.xpath("//option[@value='1247']"));
+		option.click();
+		
+		// Close the dropdown
+		driver.findElement(By.xpath("//body")).click();
 		Thread.sleep(3000);
 		
 		//select2-ddlpolicy-result-ib7e-1209
